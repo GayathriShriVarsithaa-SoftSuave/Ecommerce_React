14c5f9c7532e054b42b12bbd85b060f32d6a77a5
GayathriShriVarsithaa <gayathrishri.varsithaa@softsuave.org>
2026-04-10 12:08:45 +0530

    Moved the cartitem storage to session storage

--- src/features/cartitem/cartitemSlice.tsx
+++ src/features/cartitem/cartitemSlice.tsx
@@ -12,19 +12,19 @@ interface CartItemState{
     cartitems:CartItemMap
 }
 const existingCartItems: CartItemMap = {};
-Object.keys(localStorage).forEach((key) => {
-  existingCartItems[key] = JSON.parse(localStorage.getItem(key) || '{}');
+Object.keys(sessionStorage).forEach((key) => {
+  existingCartItems[key] = JSON.parse(sessionStorage.getItem(key) || '{}');
 });
 const initialState: CartItemState = {
   cartitems: existingCartItems,
 };
 const cartitemSlice = createSlice({name:"cartitem",initialState,reducers: {
     addCartItem: (state, action: PayloadAction<{ id: string; item: CartItem }>) => {
-        localStorage.setItem(action.payload.id, JSON.stringify(action.payload.item));
+        sessionStorage.setItem(action.payload.id, JSON.stringify(action.payload.item));
         state.cartitems[action.payload.id] = action.payload.item;
     },
     deleteCartItem: (state, action: PayloadAction<string>) => {
-        localStorage.removeItem(action.payload);
+        sessionStorage.removeItem(action.payload);
         delete state.cartitems[action.payload];
     }
 }});
